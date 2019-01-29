3038c3cc6615004e49f5bee5aef36d603c5a9a6f
Richard Evaristo <richardevaristo@Cheeper.local>
2019-01-29 11:04:03 +0800

    Updates

---
+++ precache-manifest.7e093d97b1ce1abb2d43f1e6278d1b5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f7d67d5c3929f4d8719",
+    "url": "/github.io/react-blog-context/static/js/runtime~main.8f7d67d5.js"
+  },
+  {
+    "revision": "81972f2e543c69ad9cc8",
+    "url": "/github.io/react-blog-context/static/js/main.81972f2e.chunk.js"
+  },
+  {
+    "revision": "9d5d66d3418a5658dcf8",
+    "url": "/github.io/react-blog-context/static/js/1.9d5d66d3.chunk.js"
+  },
+  {
+    "revision": "81972f2e543c69ad9cc8",
+    "url": "/github.io/react-blog-context/static/css/main.eebb6bad.chunk.css"
+  },
+  {
+    "revision": "9d5d66d3418a5658dcf8",
+    "url": "/github.io/react-blog-context/static/css/1.9496e237.chunk.css"
+  },
+  {
+    "revision": "7e492fa4219a7ce69b9c98a23e151cde",
+    "url": "/github.io/react-blog-context/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/github.io/react-blog-context/precache-manifest.7e093d97b1ce1abb2d43f1e6278d1b5d.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/github.io/react-blog-context/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
